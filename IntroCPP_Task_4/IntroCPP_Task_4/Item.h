19b81dd08db4df4afc8185557722fa42ddca4751
Deion Trinidad <deion_trinidad@outlook.com>
2024-03-25 13:27:49 +1100

    Resetting everything
    I just need to rethink what the assessment asks

--- IntroCPP_Task_4/IntroCPP_Task_4/Item.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "String.h"
-
-class Item {
-public:
-    virtual ~Item() { } // destructor for proper cleanup
-    virtual String Description() const = 0; // Gives items descriptions
-    virtual void Use() = 0; // used for usage of item
-};
